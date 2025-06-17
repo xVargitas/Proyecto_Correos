83d2b61f88de94e7ec1340d4e09382fc540031ca
xVargitas <thovarsan@gmail.com>
2025-06-16 23:44:07 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Correos/Modelo/CentroDeDistribucion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Correos.Modelo
+{
+    public class CentroDeDistribucion
+    {
+        public string IdCentroDistribucion { get; set; }
+        public string Estado { get; set; }
+    }
+}
